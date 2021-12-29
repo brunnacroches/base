efa3dcf5af060a476931fca5dc96dd2fdf9a08d6
brunnacroches <brunnacroches@gmail.com>
2021-12-29 12:03:36 -0300

    s

--- app.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-  var server = http.createServer(function (req, res){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write("Seja bem-vindo!");
-        res.end();
-  });
-  server.listen(3000);
