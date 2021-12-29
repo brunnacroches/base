1083dbcd388cfe54159275ad681fc7f25a921d54
brunnacroches <brunnacroches@gmail.com>
2021-12-29 00:01:32 -0300

    c

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,9 +1,6 @@
 	// Find the latest version by visiting https://cdn.skypack.dev/three.
 	import * as THREE from 'https://cdn.skypack.dev/pin/three@v0.135.0-pjGUcRG9Xt70OdXl97VF/mode=imports,min/optimized/three.js';
 
-	import { OrbitControls } from 'https://cdn.skypack.dev/three@0.135.0/examples/jsm/controls/OrbitControls.js';
-
-
 	import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.135.0/examples/jsm/loaders/GLTFLoader.js';
 
 
